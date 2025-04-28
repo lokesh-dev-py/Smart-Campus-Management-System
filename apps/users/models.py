bd5c5da6f3359d5aeae2117d652fa3122f79b9d7
Lokesh-DataScience <lokeshkumar.engineer@gmail.com>
2025-04-28 13:55:59 +0530

    Create model for users app.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,60 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        # creates and saves a User with the given email and password
+        if not email:
+            raise ValueError('The Email field must be set')
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        #create and save a superuser with the given email and password
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_teacher = True
+        user.is_student = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        #only superuser has all permissions
+        return self.is_superuser
+    
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        #only superuser has all permissions
+        return self.is_superuser
