f432d739e28e5b1d45614d41cd57d4f4c0bf3854
Lokesh-DataScience <lokeshkumar.engineer@gmail.com>
2025-04-28 14:08:23 +0530

    Registered the model into admin pannel.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
 # Register your models here.
+
+@admin.register(User)
+class UserModelAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'name', 'is_staff', 'is_active', 'is_teacher', 'is_student')
+    list_filter = ('is_staff', 'is_active', 'is_teacher', 'is_student')
+    search_fields = ('email', 'name')
+    filter_horizontal = ()
+    fieldsets = [
+        ('User Credentials', {'fields': ['email', 'password']}),
+        ('Personal Info', {'fields': ['name']}),
+        ('Permissions', {'fields': ['is_staff', 'is_active', 'is_teacher', 'is_student']}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2')
+            }
+        )
+    ]
+
